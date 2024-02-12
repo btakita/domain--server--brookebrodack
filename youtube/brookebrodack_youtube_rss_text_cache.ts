eb8fa07cd61c52ce5ee6e058900343c4d84198af
Brian Takita <brian.takita@gmail.com>
2024-02-12 14:40:40 -0500

    ./youtube: more efficient caching:
    	+ brookebrodack_youtube_rss_text_cache_meta:
    		+ brookebrodack_youtube_rss_text_cache_meta$_
    		+ brookebrodack_youtube_rss_text_cache_meta_
    	brookebrodack_youtube_rss: ∋ brookebrodack_youtube_rss_text_cache
    	brookebrodack_youtube_video_a1: ∋ brookebrodack_youtube_rss_text_cache

--- youtube/brookebrodack_youtube_rss_text_cache.ts
+++ youtube/brookebrodack_youtube_rss_text_cache.ts
@@ -7,6 +7,23 @@ export const brookebrodack_youtube_rss__url = 'http://www.youtube.com/feeds/vide
 // 1 hour
 const cache_ttl_ms = 60 * 3600 * 1000
 export const [
+	brookebrodack_youtube_rss_text_cache_meta$_,
+	brookebrodack_youtube_rss_text_cache_meta_
+] = id_be_memo_pair_(
+	'brookebrodack_youtube_rss_text_cache_meta',
+	(ctx:wide_ctx_T&wide_app_ctx_T)=>
+		drizzle_db_(ctx)
+			.select({
+				text_cache_id: text_cache.text_cache_id,
+				create_ms: text_cache.create_ms,
+				validate_ms: text_cache.validate_ms,
+			})
+			.from(text_cache)
+			.where(eq(text_cache.text_cache_id, brookebrodack_youtube_rss__url))
+			.limit(1)
+			.all()[0]
+)
+export const [
 	brookebrodack_youtube_rss_text_cache$_,
 	brookebrodack_youtube_rss_text_cache_,
 ] = id_be_memo_pair_<
@@ -17,41 +34,46 @@ export const [
 	'brookebrodack_youtube_rss',
 	(ctx, brookebrodack_youtube_rss$)=>{
 		const db = drizzle_db_(ctx)
-		const rss_text_cache =
-			db.select()
-				.from(text_cache)
-				.where(eq(text_cache.text_cache_id, brookebrodack_youtube_rss__url))
-				.limit(1)
-				.all()[0]
+		const brookebrodack_youtube_rss_text_cache_meta = brookebrodack_youtube_rss_text_cache_meta_(ctx)
 		if (
-			!rss_text_cache?.text_cache_id
-			|| rss_text_cache.validate_ms.getTime() + cache_ttl_ms < new Date().getTime()
+			!brookebrodack_youtube_rss_text_cache_meta?.text_cache_id
+			|| brookebrodack_youtube_rss_text_cache_meta.validate_ms.getTime() + cache_ttl_ms < new Date().getTime()
 		) {
 			run(async ()=>{
 				const response = await fetch(brookebrodack_youtube_rss__url)
 				const rss = await response.text()
-				const brookebrodack_youtube_rss_text_cache = db.insert(text_cache)
-					.values({
-						text_cache_id: brookebrodack_youtube_rss__url,
-						data: rss
-					}).onConflictDoUpdate({
-						target: text_cache.text_cache_id,
-						set: {
-							...(
-								rss !== rss_text_cache?.data
-									? {
-										create_ms: sql`CURRENT_TIMESTAMP`,
-										data: rss
-									}
-									: null),
-							validate_ms: sql`CURRENT_TIMESTAMP`,
-						}
-					})
-					.returning()
-					.get()
-				brookebrodack_youtube_rss$._ = brookebrodack_youtube_rss_text_cache
+				const data_o = db.select({ data: text_cache.data })
+					.from(text_cache)
+					.where(eq(text_cache.text_cache_id, brookebrodack_youtube_rss__url))
+					.limit(1)
+					.all()[0]
+				brookebrodack_youtube_rss$._ =
+					db.insert(text_cache)
+						.values({
+							text_cache_id: brookebrodack_youtube_rss__url,
+							data: rss
+						})
+						.onConflictDoUpdate({
+							target: text_cache.text_cache_id,
+							set: {
+								...(
+									rss !== data_o?.data
+										? {
+											create_ms: sql`CURRENT_TIMESTAMP`,
+											data: rss
+										}
+										: null),
+								validate_ms: sql`CURRENT_TIMESTAMP`,
+							}
+						})
+						.returning()
+						.get()
 			}).then()
 			return null
 		}
-		return rss_text_cache
+		return db.select()
+			.from(text_cache)
+			.where(eq(text_cache.text_cache_id, brookebrodack_youtube_rss__url))
+			.limit(1)
+			.all()[0]
 	})
--- youtube/brookebrodack_youtube_video_a1.ts
+++ youtube/brookebrodack_youtube_video_a1.ts
@@ -1,10 +1,13 @@
 import { youtube_video } from '@rappstack/domain--server--youtube/schema'
 import { drizzle_db_ } from '@rappstack/domain--server/drizzle'
-import { compact } from 'ctx-core/all'
-import { id_be_memo_pair_, nullish__none_, type wide_ctx_T } from 'ctx-core/rmemo'
+import { compact, rmemo__wait } from 'ctx-core/all'
+import { id_be_memo_pair_, type wide_ctx_T } from 'ctx-core/rmemo'
 import { eq } from 'drizzle-orm'
 import { type wide_app_ctx_T } from 'relysjs/server'
-import { brookebrodack_youtube_rss_text_cache_ } from './brookebrodack_youtube_rss_text_cache.js'
+import {
+	brookebrodack_youtube_rss_text_cache$_,
+	brookebrodack_youtube_rss_text_cache_meta_
+} from './brookebrodack_youtube_rss_text_cache.js'
 export const [
 	brookebrodack_youtube_video_a1$_,
 	brookebrodack_youtube_video_a1_,
@@ -14,131 +17,128 @@ export const [
 	wide_ctx_T&wide_app_ctx_T
 >(
 	'brookebrodack_youtube_video_a1',
-	(ctx, brookebrodack_youtube_video_a1$)=>
-		nullish__none_([brookebrodack_youtube_rss_text_cache_(ctx)],
-			brookebrodack_youtube_rss_text_cache=>{
-				const db = drizzle_db_(ctx)
-				const [up_to_date] =
-					db.select()
-						.from(youtube_video)
-						.where(eq(youtube_video.create_ms, brookebrodack_youtube_rss_text_cache.create_ms))
-						.limit(1)
-						.all()
-				if (!up_to_date) {
-					console.info('brookebrodack_youtube_video_a1|CACHE MISS')
-					upsert()
-						.then(brookebrodack_youtube_video_a1=>{
-							brookebrodack_youtube_video_a1$._ = compact(
-								brookebrodack_youtube_video_a1) as typeof youtube_video.$inferSelect[]
-						})
-					return null
-				}
-				return (
-					db.select()
-						.from(youtube_video)
-						.all()
-				)
-				async function upsert():Promise<(typeof youtube_video.$inferSelect|null)[]> {
-					let _youtube_video:Partial<typeof youtube_video.$inferSelect> = {}
-					const rewriter = new HTMLRewriter()
-					rewriter.on('videoId', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.videoId = el.text
-						}
-					})
-					rewriter.on('entry channelId', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.channelId = el.text
-						}
-					})
-					rewriter.on('entry author name', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.author_name = el.text
-						}
-					})
-					rewriter.on('entry author uri', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.author_uri = el.text
-						}
-					})
-					rewriter.on('entry published', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.published_ms = new Date(el.text)
-						}
-					})
-					rewriter.on('entry updated', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.updated_ms = new Date(el.text)
-						}
-					})
-					rewriter.on('entry title', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.title = el.text
-						}
-					})
-					rewriter.on('entry thumbnail', {
-						element(el) {
-							_youtube_video.thumbnail = el.getAttribute('url')!
-						}
-					})
-					rewriter.on('entry description', {
-						text(el) {
-							if (!el.lastInTextNode) _youtube_video.description = el.text
-						}
-					})
-					const brookebrodack_youtube_video_promise_a1:Promise<typeof youtube_video.$inferSelect|null>[] = []
-					let count = 0
-					rewriter.on('entry', {
-						element(el) {
-							brookebrodack_youtube_video_promise_a1.push(
-								new Promise(resolve=>{
-									el.onEndTag(()=>{
-										if (
-											_youtube_video.videoId
-											&& _youtube_video.channelId
-											&& _youtube_video.author_name
-											&& _youtube_video.author_uri
-											&& _youtube_video.title
-											&& _youtube_video.thumbnail
-										) {
-											_youtube_video.create_ms = brookebrodack_youtube_rss_text_cache.create_ms
-											const __youtube_video_a1 =
-												db.insert(youtube_video)
-													.values(_youtube_video as typeof youtube_video.$inferSelect)
-													.onConflictDoUpdate({
-														target: youtube_video.videoId,
-														set: _youtube_video
-													})
-													.returning()
-													.all()
-											resolve((__youtube_video_a1 as never)[0] as typeof youtube_video.$inferSelect)
-										}
-										else {
-											console.warn('brookebrodack_youtube_video_a1|entry|onEndTag|missing props', {
-												videoId: _youtube_video.videoId,
-												count: ++count,
-												_youtube_video,
-											})
-											resolve(null)
-										}
-										_youtube_video = {}
+	(ctx, brookebrodack_youtube_video_a1$)=>{
+		const brookebrodack_youtube_rss_text_cache_meta = brookebrodack_youtube_rss_text_cache_meta_(ctx)
+		const db = drizzle_db_(ctx)
+		const [up_to_date] =
+			db.select()
+				.from(youtube_video)
+				.where(eq(youtube_video.create_ms, brookebrodack_youtube_rss_text_cache_meta.create_ms))
+				.limit(1)
+				.all()
+		if (!up_to_date) {
+			console.info('brookebrodack_youtube_video_a1|CACHE MISS')
+			upsert()
+				.then(brookebrodack_youtube_video_a1=>{
+					brookebrodack_youtube_video_a1$._ = compact(
+						brookebrodack_youtube_video_a1) as typeof youtube_video.$inferSelect[]
+				})
+			return null
+		}
+		return (
+			db.select()
+				.from(youtube_video)
+				.all()
+		)
+		async function upsert():Promise<(typeof youtube_video.$inferSelect|null)[]> {
+			let _youtube_video:Partial<typeof youtube_video.$inferSelect> = {}
+			const rewriter = new HTMLRewriter()
+			rewriter
+				.on('videoId', text_ontext_('videoId'))
+				.on('entry channelId', text_ontext_('channelId'))
+				.on('entry author name', text_ontext_('author_name'))
+				.on('entry author uri', text_ontext_('author_uri'))
+				.on('entry published', date_ontext_('published_ms'))
+				.on('entry updated', date_ontext_('updated_ms'))
+				.on('entry title', text_ontext_('title'))
+				.on('entry thumbnail', {
+					element(el) {
+						_youtube_video.thumbnail = el.getAttribute('url')!
+					}
+				})
+				.on('entry description', text_ontext_('description'))
+			const brookebrodack_youtube_video_promise_a1:Promise<typeof youtube_video.$inferSelect|null>[] = []
+			let count = 0
+			rewriter.on('entry', {
+				element(el) {
+					brookebrodack_youtube_video_promise_a1.push(
+						new Promise(resolve=>{
+							el.onEndTag(async ()=>{
+								if (
+									_youtube_video.videoId
+									&& _youtube_video.channelId
+									&& _youtube_video.author_name
+									&& _youtube_video.author_uri
+									&& _youtube_video.title
+									&& _youtube_video.thumbnail
+								) {
+									const local__youtube_video = _youtube_video
+									rmemo__wait(
+										brookebrodack_youtube_rss_text_cache$_(ctx),
+										brookebrodack_youtube_rss_text_cache=>brookebrodack_youtube_rss_text_cache,
+										10_000
+									).then(brookebrodack_youtube_rss_text_cache=>{
+										local__youtube_video.create_ms = brookebrodack_youtube_rss_text_cache!.create_ms
+										const upsert__youtube_video_a1 =
+											db.insert(youtube_video)
+												.values(local__youtube_video as typeof youtube_video.$inferSelect)
+												.onConflictDoUpdate({
+													target: youtube_video.videoId,
+													set: local__youtube_video
+												})
+												.returning()
+												.all()
+										resolve((upsert__youtube_video_a1 as never)[0] as typeof youtube_video.$inferSelect)
+									}).catch(err=>{
+										console.error(err)
+										resolve(null)
 									})
-								})
-							)
-						}
-					})
-					const response = rewriter.transform(
-						new Response(
-							brookebrodack_youtube_rss_text_cache.data
-								.replaceAll('<yt:', '<')
-								.replaceAll('</yt:', '</')
-								.replaceAll('<media:', '<')
-								.replaceAll('</media:', '</')
-						))
-					await consume(response.body!)
-					return Promise.all(brookebrodack_youtube_video_promise_a1)
+								}
+								else {
+									console.warn('brookebrodack_youtube_video_a1|entry|onEndTag|missing props', {
+										videoId: _youtube_video.videoId,
+										count: ++count,
+										_youtube_video,
+									})
+									resolve(null)
+								}
+								_youtube_video = {}
+							})
+						})
+					)
 				}
-			}))
+			})
+			const brookebrodack_youtube_rss_text_cache =
+				(await rmemo__wait(
+					brookebrodack_youtube_rss_text_cache$_(ctx),
+					brookebrodack_youtube_rss_text_cache=>brookebrodack_youtube_rss_text_cache,
+					10_000))!
+			const response = rewriter.transform(
+				new Response(
+					brookebrodack_youtube_rss_text_cache.data
+						.replaceAll('<yt:', '<')
+						.replaceAll('</yt:', '</')
+						.replaceAll('<media:', '<')
+						.replaceAll('</media:', '</')
+				))
+			await consume(response.body!)
+			return Promise.all(brookebrodack_youtube_video_promise_a1)
+			function text_ontext_(key:Exclude<keyof typeof _youtube_video, 'create_ms'|'published_ms'|'updated_ms'>) {
+				return {
+					text(el:HTMLRewriterTypes.Text) {
+						if (!el.lastInTextNode) _youtube_video[key] = el.text
+					}
+				}
+			}
+			function date_ontext_(key:Extract<keyof typeof _youtube_video, 'create_ms'|'published_ms'|'updated_ms'>) {
+				return {
+					text(el:HTMLRewriterTypes.Text) {
+						if (!el.lastInTextNode) _youtube_video[key] = new Date(el.text)
+					}
+				}
+			}
+		}
+	})
 async function consume(stream:ReadableStream) {
 	const reader = stream.getReader()
 	while (!(await reader.read()).done) { /* NOOP */
